d943449482686a2d1a1ea06b7f2a8df985674f8f
cmena <cmena@newroads.org>
2016-09-23 12:33:11 -0400

    TestLoops

---
+++ TestLoops2.java
@@ -0,0 +1,13 @@
+public class TestLoops2{
+  public static void main(String[] args){
+    
+    int x = 2;
+    
+    for(int index = 0; index < 10; index++){
+      
+      System.out.println(x*index);
+    }
+    
+  }
+  
+}
