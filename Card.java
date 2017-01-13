d71266626024e2d381dc24b81e7c2810f5a0ee7f
cmena <cmena@newroads.org>
2017-01-13 12:09:55 -0500

    TestCard update

--- Card.java
+++ Card.java
@@ -1,9 +1,10 @@
 public class Card{
   
+  
   //first - declare the properties/members, aka info/details about the object
-  String rank;
-  String color;
-  String suit;
+  private String rank;
+  private String color;
+  private String suit;
   
   //second - create a constructor
   public Card(){
@@ -11,6 +12,30 @@ public class Card{
     color = "black";
     suit = "club";
     
+  }
+  
+  //getters and setters - mutators and accessors
+  
+  //getters
+  public String getRank(){
+    return rank;
+  }
+  public String getColor(){
+    return color;
+  }
+  public String getSuit(){
+    return suit;
+  }
+  
+  //setter
+  public void setRank(String newRank){
+    rank = newRank;
+  }
+  public void setColor(String newColor){
+    color = newColor;
+  }
+  public void setSuit(String newSuit){
+    suit = newSuit;
   }
   
   //outputs details of the object
--- TestCard.java
+++ TestCard.java
@@ -5,9 +5,20 @@ public class TestCard{
     
     //create Card object
     Card c1 = new Card();
+    Card c2 = new Card();
     
     //print out card object
     System.out.println(c1);
+    
+    c1.setRank("J");
+    
+    System.out.println(c1);
+    
+    c2.setRank("K");
+    c2.setColor("red");
+    c2.setSuit("heart");
+    
+    System.out.println(c2);
     
   }//end of main method
   
