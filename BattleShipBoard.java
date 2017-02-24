f7640aa66908658113c85528f0fd08b3e0bae3f0
cmena <cmena@newroads.org>
2017-02-24 12:29:26 -0500

    new board changes in progress

--- BattleShipBoard.java
+++ BattleShipBoard.java
@@ -1,11 +1,12 @@
 public class BattleShipBoard{
 
   private char[][] board = {
-                              {' ', ' ', ' ', ' ', ' '},
-                              {' ', ' ', ' ', ' ', ' '},
-                              {' ', ' ', ' ', ' ', ' '},
-                              {' ', ' ', ' ', ' ', ' '},
-                              {' ', ' ', ' ', ' ', ' '}
+                              {' ', ' ', ' ', ' ', ' ', ' '},
+                              {' ', ' ', ' ', ' ', ' ', ' '},
+                              {' ', ' ', ' ', ' ', ' ', ' '},
+                              {' ', ' ', ' ', ' ', ' ', ' '},
+                              {' ', ' ', ' ', ' ', ' ', ' '},
+                              {' ', ' ', ' ', ' ', ' ', ' '}
                            };
   
   public BattleShipBoard(){
@@ -17,15 +18,16 @@ public class BattleShipBoard{
   }
   
   public void setupShips(int x, int y){
-    board[x][y] = '.'; 
+    board[x][y] = '*'; 
   }
   
   public void printBoard(){
-    System.out.println(" | " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " | " + board[0][3] + " | " + board[0][4]  + " | ");
-    System.out.println(" | " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " | " + board[1][3] + " | " + board[1][4]  + " | ");
-    System.out.println(" | " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " | " + board[2][3] + " | " + board[2][4]  + " | ");
-    System.out.println(" | " + board[3][0] + " | " + board[3][1] + " | " + board[3][2] + " | " + board[3][3] + " | " + board[3][4]  + " | ");
-    System.out.println(" | " + board[4][0] + " | " + board[4][1] + " | " + board[4][2] + " | " + board[4][3] + " | " + board[4][4]  + " | ");
+    System.out.println("   " + board[0][0] + " 1 " + board[0][1] + " 2 " + board[0][2] + " 3 " + board[0][3] + " 4 " + board[0][4]  + " 5 ");
+    System.out.println("   1 " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " | " + board[1][3] + " | " + board[1][4]  + " | ");
+    System.out.println("   2 " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " | " + board[2][3] + " | " + board[2][4]  + " | ");
+    System.out.println("   3 " + board[3][0] + " | " + board[3][1] + " | " + board[3][2] + " | " + board[3][3] + " | " + board[3][4]  + " | ");
+    System.out.println("   4 " + board[4][0] + " | " + board[4][1] + " | " + board[4][2] + " | " + board[4][3] + " | " + board[4][4]  + " | ");
+    System.out.println("   5 " + board[5][0] + " | " + board[5][1] + " | " + board[5][2] + " | " + board[5][3] + " | " + board[5][4]  + " | ");
   }
   
 }
